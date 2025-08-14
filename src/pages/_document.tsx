88a9d22350aa36d7e52ae0e3fbde6e2277c2c774
Ali Samadi <alisamadi.096@gmail.com>
2025-08-14 10:36:35 -0500

    Initial commit

---
+++ src/pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+import { AppConfig } from "../utils/AppConfig";
+
+// Need to create a custom _document because i18n support is not compatible with `next export`.
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang={AppConfig.locale}>
+        <Head>
+          <title>OxinnGlobal</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
